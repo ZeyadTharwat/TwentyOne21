2144763a3011785492ae17d888a07a9bb904bb58
a1ee7 <131653037+a1ee7@users.noreply.github.com>
2024-09-15 00:27:14 +0300

    Update OurHistory.tsx Ali 1

--- src/components/About/OurHistory.tsx
+++ src/components/About/OurHistory.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import HistoryImg from "../../assets/Govind - Head shot.png"
 import ButtonVector from "../../assets/ButtonVector.svg"
 import Historybg from "../../assets/HistoryBg.png"
@@ -16,10 +17,14 @@ const OurHistory = () => {
             </p>
             <p className="font-houschka sm:text-xl text-sm font-light sm:mb-5 mb-8">Shepley’s vision, coupled with his commitment to pushing boundaries, ensures that no two projects are alike, each crafted to create a distinct brand identity. As a designer and business owner, Shepley is dedicated to personal and professional growth, fostering an environment where innovation thrives and his team excels. Today, TwentyOne06 continues to expand, breaking into new industries such as hotel design and sustainable development, while always remaining focused on delivering groundbreaking, globally recognized work.
             </p>
+          
+          <Link to="/contact-us">
 <button className="relative">
                 <span className="font-houschka font-light uppercase xl:text-lg sm:text-base text-[10px] absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-full">Contact Us</span>
                 <img height={180} width={180} className='scale-x-[-1] xl:size-auto size-[90px] sm:size-[140px]' src={ButtonVector} alt="Button Vector" />
                 </button>
+            </Link>
+
         </div>
     </div>
     </div>
