9b5e1d5ebe693730ba362b60859daa8d2f7abfbc
a1ee7 <131653037+a1ee7@users.noreply.github.com>
2024-09-15 00:09:17 +0300

    Update AboutAgency.tsx Ali

--- src/components/Home/AboutAgency.tsx
+++ src/components/Home/AboutAgency.tsx
@@ -11,8 +11,8 @@ const AboutAgency = () => {
           <img src={AboutImages} className="xl:max-w-[606px] sm:max-w-[506px] max-h-[623px] object-cover" alt="" />
         </div>
         <div className="md:text-start text-center">
-          <h1 className="xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-vissa uppercase tracking-[0.02em] sm:mb-8 mb-4">A Behind-the-Scenes Look at Our Agency</h1>
-          <p className="font-houschka font-light text-[#D0D0D0] sm:text-lg text-sm md:mb-16 mb-12 md:pb-1.5">a voluptatibus quaerat doloribus pariatur eum sequi molestiae molestiae iure. Sed magni totam nulla sit optio. Facilis ad mollitia A voluptatibus quaerat doloribus pariatur eum sequi molestiae molestiae iure. Sed A voluptatibus quaerat doloribus pariatur eum sequi molestiae molestiae iure. Sed magni totam nulla sit optio. Facilis ad mollitia A voluptatibus quaerat doloribus pariatur eum sequi molestiae molestiae iure. Sed </p>
+          <h1 className="xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-vissa uppercase tracking-[0.02em] sm:mb-8 mb-4">Exclusivity Meets Innovation</h1>
+          <p className="font-houschka font-light text-[#D0D0D0] sm:text-lg text-sm md:mb-16 mb-12 md:pb-1.5">At the intersection of creativity and innovation, TwentyOne06 is a design agency redefining the boundaries of contemporary hospitality and commercial spaces. Based in Dubai, with a global reach, we specialize in crafting experiences that merge form with function, offering bespoke design solutions that are as innovative as they are timeless.</p>
           <Link className="lg:block hidden" to={'/about'}>
             <button className="relative">
               <span className="font-houschka font-light uppercase xl::text-lg absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-full">Learn More</span>
