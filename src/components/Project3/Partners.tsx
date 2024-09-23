4c852b39e4c7c813f52341f24ee648c2bcbb2fcc
a1ee7 <131653037+a1ee7@users.noreply.github.com>
2024-09-23 04:10:48 +0300

    Update Partners.tsx TERO

--- src/components/Project3/Partners.tsx
+++ src/components/Project3/Partners.tsx
@@ -1,6 +1,9 @@
-import Partner1 from "../../assets/Voco/CID MENA - WINNER.png";
-import Partner2 from "../../assets/Voco/HEART OF EUROPE.png";
-import Partner3 from "../../assets/Voco/VOCO MONACO.png";
+import Partner1 from "../../assets/tero/CATERER MIDDLE EAST.png";
+import Partner2 from "../../assets/tero/DESIGN ET AL.png";
+import Partner3 from "../../assets/tero/FACT.png";
+import Partner4 from "../../assets/tero/FRAME.png";
+import Partner5 from "../../assets/tero/MICHELIN.png";
+
 const Partners = () => {
     return (
         <div className="w-full sm:h-[200px] h-[250px] bg-[#1A1A1A] relative">
@@ -17,6 +20,12 @@ const Partners = () => {
                         </div>
                         <div className="col-span-1">
                             <img className="mx-auto md:max-w-[150px] max-w-[100px] basis-1/4" src={Partner3} alt="Partner 3" />
+                        </div>
+                        <div className="col-span-1">
+                            <img className="mx-auto md:max-w-[150px] max-w-[100px] basis-1/4" src={Partner4} alt="Partner 4" />
+                        </div>
+                        <div className="col-span-1">
+                            <img className="mx-auto md:max-w-[150px] max-w-[100px] basis-1/4" src={Partner5} alt="Partner 5" />
                         </div>
                     </div>
                 </div>
