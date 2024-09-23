54cd22bfaf78856d1eeb4ccfe2aa22db7a66a242
a1ee7 <131653037+a1ee7@users.noreply.github.com>
2024-09-23 22:13:00 +0300

    Update PressAndReports.tsx 1

--- src/components/Home/PressAndReports.tsx
+++ src/components/Home/PressAndReports.tsx
@@ -88,7 +88,7 @@ const PressAndReports = () => {
                 >
                     {pressItems.map((item, index) => (
                         <SwiperSlide key={index}>
-                            <Link to={item.article}>
+                            <Link to={item.article} target="_blank">
                                 <img src={item.img} className="w-full xl:min-h-[343px] md:min-h-[270px] md:max-h-[270px] xl:max-h-[343px] object-cover mb-3" alt={item.title} />
                                 <h1 className="md:text-[40px] sm:text-3xl   font-vissa uppercase mt-2 sm:mt-4">{item.title}</h1>
                                 <p className="text-[#8D8D8D] font-houschka font-light sm:text-base text-[8px] sm:mb-3 mb-1.5 mt-1 sm:mt-2">{item.description}</p>
