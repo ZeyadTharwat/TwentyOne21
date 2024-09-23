a1d159d54a50e97fc7285341ce6bba49e7550bc4
a1ee7 <131653037+a1ee7@users.noreply.github.com>
2024-09-23 04:11:21 +0300

    Update Partners.tsx Fix 2

--- src/components/Projects5/Partners.tsx
+++ src/components/Projects5/Partners.tsx
@@ -20,12 +20,6 @@ const Partners = () => {
                         <div className="col-span-1 sm:max-w-[300px] max-w-[200px]">
                             <img className="mx-auto" src={Partner3} alt="Partner 3" />
                         </div>
-                        <div className="col-span-1 sm:max-w-[300px] max-w-[200px]">
-                            <img className="mx-auto" src={Partner4} alt="Partner 4" />
-                        </div>
-                        <div className="col-span-1 sm:max-w-[300px] max-w-[200px]">
-                            <img className="mx-auto" src={Partner5} alt="Partner 5" />
-                        </div>
                     </div>
                 </div>
             </div>
