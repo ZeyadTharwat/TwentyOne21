a952f7d10c8e88af8b6ba97cf984571c477b98f3
a1ee7 <131653037+a1ee7@users.noreply.github.com>
2024-09-23 03:36:09 +0300

    Update Partners.tsx 1

--- src/components/Projects4/Partners.tsx
+++ src/components/Projects4/Partners.tsx
@@ -9,10 +9,10 @@ const Partners = () => {
             <div className="h-full gradient-bg flex items-center justify-center relative " >
                 <div className="relative z-[9999999] container mx-1">
                     <div className="flex justify-center lg:gap-10 sm:gap-5 gap-x-4 gap-y-6 items-center scale-x-[-1]">
-                    <div className="md:max-w-[400px] max-w-[250px]">
+                    <div className="col-span-1 max-w-[250px] sm:max-w-[200px] basis-1/4">
                             <img  src={Partner1} alt="Partner 1" />
                         </div>
-                    <div className="md:max-w-[400px] max-w-[250px]">
+                    <div className="col-span-1 max-w-[250px] sm:max-w-[200px] basis-1/4">
                             <img  src={Partner2} alt="Partner 2" />
                         </div>
                     </div>
